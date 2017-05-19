234c02bd00d8865efc6c6327c97b82c1e795fa60
Petro Baykovskyy <baykovskyy@gmail.com>
2017-05-19 18:17:03 +0300

    JS-Homework

---
+++ script.js
@@ -0,0 +1,8 @@
+var name = prompt('What is your Name?');
+var event = prompt('That type of event would you prefer?');
+var time = prompt('What day is the best for you?');
+var city = prompt('In what city do you want to have this event?');
+console.log('So, ' + name + ' let us go to ' + event + ' in ' + city + ' on ' + time + ' at 22.00 ');
+
+
+
